f03d67239c2627ca3f7c3429648cf1a751a6a71d
NidaWorld <160415456+nidakhan6@users.noreply.github.com>
2024-03-07 18:20:02 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,68 @@
+let username1: string= "Nida";
+console.log(username1);
+
+let username2 :string = "NidaFatima";
+console.log(username2);
+
+let address: string ="Garden";
+console.log(address);
+
+let city: string="Karachi"
+console.log(city);
+
+let age : number =20
+console.log(age);
+
+let engage:boolean = false
+console.log(engage);
+
+let favorite_holybook: string= "Quran Majeed";
+console.log(favorite_holybook);
+
+let Education: string = "intermidiate of commerce";
+console.log(Education);
+
+let favoriteColor: string ="Blue";
+console.log(favoriteColor);
+
+let favoriteDish:string="Biryani";
+console.log(favoriteDish);
+
+let dream:string="Touching Kaba";
+console.log(dream);
+
+let Hobbies: string="Outing";
+console.log(Hobbies);
+
+let favorite_Cricketer:string="Babar Azam";
+console.log(favorite_Cricketer);
+
+let interest: string="Coding";
+console.log(interest);
+
+let Bestfriend: string="Sonia";
+console.log(Bestfriend);
+
+let favorite_Sir: string ="Haseeb";
+console.log(favorite_Sir);
+
+let favorite_app: string ="instagram";
+console.log(favorite_app);
+
+let myschoolName: string ="Haseeb Public School";
+console.log(myschoolName);
+
+let goodbye: string="false";
+console.log(goodbye);
+
+
+
+
+
+
+
+
+
+
+
+
